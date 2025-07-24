b28cef14c07f31de529cd004d37a2bf849e0efcb
[ghyslain12] <[ghyslain12@gmail.com]>
2025-07-24 02:45:42 +0200

    cicd

--- project/tests/Feature/ExampleTest.php
+++ project/tests/Feature/ExampleTest.php
@@ -12,14 +12,14 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-       /* 
+       /*
         $this->withoutExceptionHandling();
 
         dump(env('APP_KEY'));
         dump(config('app.env'));
         dump(base_path());
         $this->assertTrue(true);*/
-        
-        $this->assertTrue(true);
-    }    
+
+        $this->assertTrue(false);
+    }
 }
